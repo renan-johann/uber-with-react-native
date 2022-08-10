b39694c4d26cf5609ed4436c7510952a032e3d86
Renan dos Santos <renan.santos@kk.pt>
2022-08-10 16:49:21 +0100

    It was coded store.js and navSlice.js with redux

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { React } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 //set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's build UBER!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Let's build UBER!</Text>
+      </View>
+    </Provider>
   );
 }
 
