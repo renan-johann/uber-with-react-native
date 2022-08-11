290ad5c9852e02122ee6ad24a2648470a7eb7e39
Renan dos Santos <renan.santos@kk.pt>
2022-08-11 16:45:54 +0100

    It was create stack navigator at app.js

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import { React } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+import 'react-native-gesture-handler';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { store } from './store';
 
+
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name='HomeScreen' 
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name='MapScreen' 
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
