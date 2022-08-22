b5a8bf6b53afdb02c23c612fa7be3d7f42c26147
Renan dos Santos <renan.santos@kk.pt>
2022-08-22 15:51:47 +0100

    Building the NavigateCard component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { React } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -8,31 +8,37 @@ import 'react-native-gesture-handler';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
 import { store } from './store';
+import tw from 'twrnc';
 
 export default function App() {
-
   const Stack = createStackNavigator();
 
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen 
-              name='HomeScreen' 
-              component={HomeScreen}
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen 
-              name='MapScreen' 
-              component={MapScreen}
-              options={{
-                headerShown: false,
-              }}
-            />
-          </Stack.Navigator>
+          <KeyboardAvoidingView 
+              behavior={Platform.OS === 'IOS' ? 'padding' : 'haight'}
+              style={{ flex: 1 }}
+              keyboardVerticalOffset={Platform.OS === 'ios'? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen 
+                name='HomeScreen' 
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen 
+                name='MapScreen' 
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
